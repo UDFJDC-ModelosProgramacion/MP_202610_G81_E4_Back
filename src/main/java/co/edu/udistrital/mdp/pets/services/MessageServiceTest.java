a698e9a74e8b19366be4f70a528030622ee95576
javi0331 <javier.rodrig31@gmail.com>
2026-03-23 19:12:06 -0500

    Servicios y métodos

---
+++ src/main/java/co/edu/udistrital/mdp/pets/services/MessageServiceTest.java
@@ -0,0 +1,59 @@
+package co.edu.udistrital.mdp.pets.services;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import co.edu.udistrital.mdp.pets.entities.Message;
+import co.edu.udistrital.mdp.pets.repositories.MessageRepository;
+
+@ExtendWith(MockitoExtension.class)
+class MessageServiceTest {
+
+    @Mock
+    private MessageRepository messageRepository;
+
+    @InjectMocks
+    private MessageService messageService;
+
+    @Test
+    void shouldCreateMessageSuccessfully() {
+
+        Message message = new Message();
+        message.setSenderId(1L);
+        message.setRecipientId(2L);
+        message.setSubject("Hola");
+        message.setContent("Mensaje de prueba");
+
+        when(messageRepository.save(any(Message.class)))
+                .thenReturn(message);
+
+        Message result = messageService.createMessage(message);
+
+        assertNotNull(result.getTimestamp());
+        assertFalse(result.getIsRead());
+        verify(messageRepository).save(message);
+    }
+
+    @Test
+    void shouldFailWhenSendingMessageToYourself() {
+
+        Message message = new Message();
+        message.setSenderId(1L);
+        message.setRecipientId(1L);
+        message.setSubject("Hola");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> messageService.createMessage(message)
+        );
+    }
+}
